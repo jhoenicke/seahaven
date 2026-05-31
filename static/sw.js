5e0e1edf305185086f38502239addec21ddb51cd
Jochen Hoenicke <hoenicke@gmail.com>
2026-05-31 08:47:16 +0200

    Bumped version, clean cache on activation

--- static/sw.js
+++ static/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'seahaven-v1';
+const CACHE_NAME = 'seahaven-v2';
 const FILES_TO_CACHE = [
   './',
   './online.html',
@@ -10,14 +10,23 @@ const FILES_TO_CACHE = [
   './solver.wasm'
 ];
 
-// Installation: alle Dateien cachen
+// Installation: cache all files
 self.addEventListener('install', e => {
   e.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
   );
 });
 
-// Fetch: zuerst Cache, dann Netzwerk
+// Activation: delete caches from previous versions
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+});
+
+// Fetch: serve from cache, fall back to network
 self.addEventListener('fetch', e => {
   e.respondWith(
     caches.match(e.request).then(response => response || fetch(e.request))
